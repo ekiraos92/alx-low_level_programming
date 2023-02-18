a7995ba14d60efe56b5d965cb338ccb602d44d73
ekiraos92 <ekiraos92@gmail.com>
2023-02-18 08:22:31 -0800

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - prints message
- *
+ * main - Print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *		followed by anew lint, to the standard error.
  * Returns: will return 1
  *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
