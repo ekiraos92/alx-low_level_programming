c0d58ba1712b9e0ee5d2cd130c9e24f3e028ea2f
ekiraos92 <ekiraos92@gmail.com>
2023-02-20 06:29:16 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - performs function
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
