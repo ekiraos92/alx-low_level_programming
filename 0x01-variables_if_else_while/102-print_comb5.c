367e42f1427564189309cb463b3eb4f5932cc7fc
ekiraos92 <ekiraos92@gmail.com>
2023-02-20 13:03:03 -0800

    0-99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-	int i, j; 
+	int i;
+	int j;
 
 	for (i = 0; i <= 98; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -21,7 +22,6 @@ int main(void)
 			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
 			{
-			continue;
 			putchar(',');
 			putchar(' ');
 			}
