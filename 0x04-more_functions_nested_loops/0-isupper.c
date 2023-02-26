caeb29688be074d6f8670cd5cbe91f2c863b20b4
ekiraos92 <ekiraos92@gmail.com>
2023-02-26 06:30:25 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @x: parameter
+ * Return: retuns 1 for upper and 0 otherwise
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
+
