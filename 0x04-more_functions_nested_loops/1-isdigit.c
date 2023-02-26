3e68805334135f3baade22d19dbd83b08ff24bbb
ekiraos92 <ekiraos92@gmail.com>
2023-02-26 06:35:33 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - digit checker
+ * @x: parameter
+ * Return: 1 for digit 0 otherwise
+ */
+int _isdigit(int x)
+{
+	if (x >= 40 x <=57)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
