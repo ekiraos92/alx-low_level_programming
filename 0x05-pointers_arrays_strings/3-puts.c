b7ee4440015a46b6062ab90a8653fcaa9aa58581
ekiraos92 <ekiraos92@gmail.com>
2023-03-01 20:00:16 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,13 +3,14 @@
 /**
  * _puts - prints a string
  * @str: parameter
+ * Return: lenght of string
  */
 void _puts(char *str)
 {
+	
 	while (*str != '\0')
 	{
-	_putchar(*str++);
+		_putchar(*str++);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
-
